3cfdefcc58b18e121c0c451b2afa78fb79e63d75
Emerson Rocha <rocha@ieee.org>
2022-12-26 04:13:28 -0300

    function/api-proxy: MVP

--- README.md
+++ README.md
@@ -11,6 +11,33 @@
 
 ## Functions
 
+### api-proxy
+
+- **Environment Variables**
+  - `OSM_API_DE_FACTO`: `https://www.openstreetmap.org/api/0.6`
+  - `CACHE_DRIVER`: `sqlite`
+  - `CACHE_TTL`: `3600`
+
+<!--
+
+## rebuild drill
+cd function/
+cp -r $(pwd)/* ~/Downloads/docker-build-dir
+cd ~/Downloads/docker-build-dir
+
+# docker build -t ghcr.io/fititnt/api-proxy ./api-proxy
+faas-cli build -f ./api-proxy-local.yml
+
+docker run --name api-proxy --publish 8080:8080 -d ghcr.io/fititnt/api-proxy && docker logs --follow api-proxy
+docker container stop api-proxy && docker container rm api-proxy
+
+docker container start --interactive api-proxy
+docker container start --interactive 04d37527c20728f4aab1dff6b1a02017bc9e3074fffe116d6caee76d8f57a83b
+docker logs --follow api-proxy
+
+-->
+
+
 ### api-rdf
 
 > TODO: maybe glue https://github.com/hugapi/hug/tree/develop/docker plus https://github.com/EticaAI/openstreetmap-semantic-conventions-2023/blob/main/poc/osmapi2rdfproxy.py ?
--- function/api-proxy/handler.py
+++ function/api-proxy/handler.py
@@ -1,18 +1,51 @@
-import json
-# from pprint import pprint
+import os
+import requests
+import requests_cache
 
+OSM_API_DE_FACTO = os.getenv(
+    'OSM_API_DE_FACTO', 'https://www.openstreetmap.org/api/0.6')
+CACHE_DRIVER = os.getenv('CACHE_DRIVER', 'sqlite')
+CACHE_TTL = os.getenv('CACHE_TTL', '3600')  # 1 hour
+
+# @see https://requests-cache.readthedocs.io/en/stable/
+requests_cache.install_cache(
+    'osmapi_cache',
+    # /tmp OpenFaaS allow /tmp be writtable even in read-only mode
+    # However, is not granted that changes will persist or shared
+    db_path= '/tmp/osmapi_cache.sqlite',
+    backend=CACHE_DRIVER,
+    expire_after=CACHE_TTL,
+    allowable_codes=[200, 400, 404, 500]
+)
 
 def handle(event, context):
-    return {
-        "statusCode": 200,
-        # "body": "Hello from OpenFaaS! <<" + json.dumps(context.__dict__) + '>> <<' + json.dumps(event.__dict__) + '>>'
-        "body": "Hello from OpenFaaS! <<" + repr(context.__dict__) + '>> <<' + repr(event.__dict__) + '>>'
 
-        # Maybe event['path'] return the path?
-        # https://osm-faas.etica.ai/function/api-proxy/relation/12345.ttl?1234
-        #  - event
-        #    - (...)
-        #    - 'method': 'GET'
-        #    - 'query': ImmutableMultiDict([('1234', '')])
-        #    - 'path': '/relation/12345.ttl'}
+    content = requests.get(
+        OSM_API_DE_FACTO + event.path)
+
+    # TODO: forward some hint for user ip
+    # TODO: abort know invalid requests like *.png, *.ico, *.html, ...
+
+    return {
+        "statusCode": content.status_code,
+        "headers": {
+            'content-type': content.headers['Content-Type']
+        },
+        # "body": content.text
+        "body": content.text + "\n\n" + "<!--" + repr(content.__dict__)  + '-->'
     }
+
+# def handle(event, context):
+#     return {
+#         "statusCode": 200,
+#         # "body": "Hello from OpenFaaS! <<" + json.dumps(context.__dict__) + '>> <<' + json.dumps(event.__dict__) + '>>'
+#         # "body": "Hello from OpenFaaS! <<" + repr(context.__dict__) + '>> <<' + repr(event.__dict__) + '>>'
+#         "body": repr(event.__dict__)
+#         # Maybe event['path'] return the path?
+#         # https://osm-faas.etica.ai/function/api-proxy/relation/12345.ttl?1234
+#         #  - event
+#         #    - (...)
+#         #    - 'method': 'GET'
+#         #    - 'query': ImmutableMultiDict([('1234', '')])
+#         #    - 'path': '/relation/12345.ttl'}
+#     }
